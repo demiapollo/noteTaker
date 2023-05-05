6d0ae46dc148d18cfc819676700a075c98eecb2e
demiapollo <apollodemirel67@gmail.com>
2023-05-05 10:59:11 -0700

    routes fixed

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
 //express and port
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const fs = require("fs");
+const path = require("path");
 
 //middleware parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+//routes for notes
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
+//routes for api
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    res.send(data);
+  });
+});
+
+//routes for notes api
+app.post("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) throw err;
+    const notes = JSON.parse(data);
+    const newNote = req.body;
+    notes.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+      res.json(newNote);
+    });
+  });
+});
+
+//routes for homepage
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
 //check port
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-}
-);
-
+  console.log(`Server running on port ${port}`);
+});
