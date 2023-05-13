790fed7994ac64bc6824ce24589b9096161e7e31
demiapollo <apollodemirel67@gmail.com>
2023-05-12 19:25:02 -0700

    new notes are now saved with an ID +1

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post("/api/notes", (req, res) => {
     if (err) throw err;
     const notes = JSON.parse(data);
     const newNote = req.body;
-    newNote.id = notes.length;
+    newNote.id = notes.length + 1;
     notes.push(newNote);
     fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
       res.json(newNote);
